d51e94150ccaac8674246ab7841ed32d8c85173c
Marina Sanchez <masaro1411@gmail.com>
2022-10-23 21:17:55 -0500

    Introduccion al cache storage

--- sw.js
+++ sw.js
@@ -2,13 +2,15 @@
 
 
 self.addEventListener('fatch', event => {
-    const offlineResp = new Response(`
+    //const offlineResp = new Response(`
     
-    Bienvenidoa mi Pagina Web
+    //Bienvenidoa mi Pagina Web
     
-    Disculpa, pero no puedes usarla, necesitas inernet
+   // Disculpa, pero no puedes usarla, necesitas inernet
     
-    `);
+    //`);
+
+    const offlineResp = fetch('pages/offline.html');
 
     const resp = fetch(event.request)
                         .catch( () => offlineResp);
