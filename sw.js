43396f4474b1a820df0234ecbbca51cecd3dc621
Marina Sanchez <masaro1411@gmail.com>
2022-10-24 13:44:57 -0500

    Cache dinamico:Optimizaciones

--- sw.js
+++ sw.js
@@ -1,12 +1,17 @@
 
-const CACHE_NAME = 'cache-1'
+//const CACHE_NAME = 'cache-1';
+
+const CACHE_STATIC_NAME = 'static-v1';
+const CACHE_DYNAMIC_NAME = 'dynamic-v1';
+
+const CACHE_INMUTABLE_NAME = 'inmutable-v1';
 
 
 
 self.addEventListener('install', e => {
 
 
-    const cacheProm = caches.open('CACHE_NAME')
+    const cacheProm = caches.open('CACHE_STATIC_NAME')
         .then( cache => {
 
             return cache.addAll([
@@ -14,12 +19,17 @@ self.addEventListener('install', e => {
                 '/index.html',
                 '/css/style.css',
                 '/img/main.jpg',
-                'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css',
                 '/js/app.js'
             ]);
         });
 
-    e.waitUntil( cacheProm )
+    const cacheInmutable = caches.open( CACHE_INMUTABLE_NAME )
+        .then( cache => {
+            return cache.add('https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css')
+        
+        });
+
+    e.waitUntil( Promise.all([cacheProm, cacheInmutable ]) );
 });
 
 self.addEventListener('fetch', e => {
@@ -38,7 +48,7 @@ self.addEventListener('fetch', e => {
 
             return fetch( e.request ).then( newResp => {
 
-                caches.open( CACHE_NAME ).then(cache => {
+                caches.open( CACHE_DYNAMIC_NAME ).then(cache => {
                     cache.put( e.request, newResp );
                 })
                 return newResp.clone();
