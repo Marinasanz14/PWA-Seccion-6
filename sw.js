1ad1a750b88231135a2ac24a5659acb342b2406c
Marina Sanchez <masaro1411@gmail.com>
2022-10-23 22:49:07 -0500

    Estrategia:Cache with network fallback

--- sw.js
+++ sw.js
@@ -1,13 +1,16 @@
 
+const CACHE_NAME = 'cache-1'
+
 
 
 self.addEventListener('install', e => {
 
 
-    const cacheProm = caches.open('cache-1')
+    const cacheProm = caches.open('CACHE_NAME')
         .then( cache => {
 
             return cache.addAll([
+                '/',
                 '/index.html',
                 '/css/style.css',
                 '/img/main.jpg',
@@ -17,4 +20,36 @@ self.addEventListener('install', e => {
         });
 
     e.waitUntil( cacheProm )
+});
+
+self.addEventListener('fetch', e => {
+
+
+    // 2- Cache with Network Fallback
+    const respuesta = caches.match( e.request )
+        .then( res => {
+            if ( res ) return res;
+
+
+            //No existe el archivo
+            // tengo que ir a la web
+            console.log('No existe', e.request.url);
+
+
+            return fetch( e.request ).then( newResp => {
+
+                caches.open( CACHE_NAME ).then(cache => {
+                    cache.put( e.request, newResp );
+                })
+                return newResp.clone();
+            })
+        });
+
+
+
+
+    //1- Cache Only
+    //e.respondWith( caches.match( e. request ));
 })
+
+e.respondWith( respuesta );
